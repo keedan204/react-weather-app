4bb9c60ff2ebfe4f53d2ab1c764005a679bc39ac
KeeDan204 <danikaymonique@gmail.com>
2025-08-27 18:23:37 -0500

    Added css file

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
 import React from "react";
 import Weather from "./Weather";
-import './App.css';
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <h1>Weather App</h1>
-      <Weather />
-    <footer>
-      This project was coded by {" "}<a href="https://github.com/keedan204" target="_blank" rel="noreferrer">K Daniels</a> and is <a href="https://github.com/keedan204/react-weather-app" target="_blank" rel="noreferrer">
-open-sourced on Github
-    </a>
-    </footer>
-    </div>
+        <Weather defaultCity="New York" />
+
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/keedan204"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            K Daniels
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/keedan204/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://react-weather-app-final.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
