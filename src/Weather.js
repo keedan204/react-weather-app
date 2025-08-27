b4b072d8ed12cfe2f5fa79cdb1e249783d406dd6
KeeDan204 <danikaymonique@gmail.com>
2025-08-27 18:29:36 -0500

    added multiple js and css files

--- src/Weather.js
+++ src/Weather.js
@@ -32,7 +32,7 @@ export default function Weather(props) {
   }
 
   function search() {
-    const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    const apiKey = "701f9bcct4d39645ea0eo7e3b2303a9d";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
