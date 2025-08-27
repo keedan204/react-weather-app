d1c6c53cf5a2bb5398de474d849fb9c312e8cb1a
KeeDan204 <danikaymonique@gmail.com>
2025-08-27 18:14:19 -0500

    added weather file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Weather from "./Weather";
 import './App.css';
 
 export default function App() {
@@ -7,6 +7,7 @@ export default function App() {
     <div className="App">
       <div className="container">
       <h1>Weather App</h1>
+      <Weather />
     <footer>
       This project was coded by {" "}<a href="https://github.com/keedan204" target="_blank" rel="noreferrer">K Daniels</a> and is <a href="https://github.com/keedan204/react-weather-app" target="_blank" rel="noreferrer">
 open-sourced on Github
