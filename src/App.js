2b9bcf272ddbfc5647e91a7f9f0bcae7c6fd51f4
KeeDan204 <danikaymonique@gmail.com>
2025-08-27 18:09:41 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React from "react";
+
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather App</h1>
     <footer>
       This project was coded by {" "}<a href="https://github.com/keedan204" target="_blank" rel="noreferrer">K Daniels</a> and is <a href="https://github.com/keedan204/react-weather-app" target="_blank" rel="noreferrer">
 open-sourced on Github
     </a>
     </footer>
+    </div>
     </div>
   );
 }
