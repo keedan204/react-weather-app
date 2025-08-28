437f69fbfc053f217df6a1d3c10b15c09f99a11b
KeeDan204 <danikaymonique@gmail.com>
2025-08-28 12:38:17 -0500

    debugging

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -17,10 +17,10 @@ export default function WeatherForecast(props) {
   }
 
   function load() {
-    let apiKey = "701f9bcct4d39645ea0eo7e3b2303a9d";
+    let apiKey = "1a6432c5ca7b6f9b0bee45c98d54ea71";
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?lon=${longitude}&lat=${latitude}&key=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
 
     axios.get(apiUrl).then(handleResponse);
   }
